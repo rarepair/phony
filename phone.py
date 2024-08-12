798dc362131eeb1b38961ab14885b8fe9104c44a
rarepair <wtalbotkelly@gmail.com>
2024-08-11 22:49:06 -0400

    First version of working code. Threaded play and record are functioning, as are killing of threads if handset is placed back down

---
+++ phone.py
@@ -0,0 +1,117 @@
+import logging, struct, fcntl, termios, shutil, sys, pty, os, select, errno
+from subprocess import Popen
+from time import sleep
+from select import select
+from signal import SIGTERM, SIGKILL
+from itertools import chain
+from threading import Thread, Event, current_thread
+
+class Phone():
+    def __init__(self):
+        self._logger = logging.getLogger("Phone Manager")
+        self._tjoin_timeout = 5
+        self._play_thread = None
+        self._rcrd_thread = None
+        self._play_cmd_pfx = "aplay "
+        self._rcrd_cmd_pfx = "arecord --device=plughw:1,0 --format=S24_LE --rate=48000 "
+        self._stop = Event()
+
+    # Do some magic fnctl stuff to set the virtual terminal window size of a PTY in order to match the current terminal size.
+    def _set_pty_terminal_size(self, fd):
+        columns, rows = shutil.get_terminal_size(fallback=(80, 24))
+        size = struct.pack("HHHH", rows, columns, 0, 0)
+        fcntl.ioctl(fd, termios.TIOCSWINSZ, size)
+
+    def _cmd_runner(self, cmd, stop):
+        thread_name = current_thread().name
+        logger = logging.getLogger(thread_name)
+        logger.debug("%s thread started" % thread_name)
+
+        try:
+            # Open PTYs (pseudoterminals) to use for the stdout and stderr streams. This is required because some executables will detect if they
+            # are not connected to terminals and will enable block buffering of the streams, which may not get flushed regularly with the net effect
+            # being that Python won't get the stdout/stderr messages as they are produced. The nanocom/nanokdp tools are good examples of this. Using
+            # a PTY fools the child program into thinking its being run in a terminal. We set the PTY terminal window size to match the current one
+            # trigger is being run from.
+            masters, slaves = zip(pty.openpty(), pty.openpty())
+            for fd in chain(masters, slaves): self._set_pty_terminal_size(fd)
+
+            # Start the child process, using the select module to allow for non-blocking reads of the stdout and stderr streams. Note that the order
+            # of messages between these streams cannot be guaranteed but in practice this doesn't seem to be an issue.
+            logger.info("Spawning process using command '%s'" % (cmd))
+            with Popen(args=cmd, stdin=sys.stdin, stdout=slaves[0], stderr=slaves[1], shell=True, preexec_fn=os.setsid) as proc:
+                # Close the slave file descriptors here as we won't reference them past this point.
+                for fd in slaves: os.close(fd)
+
+                # Enter a select() loop to receive stdout and stderr stream data in a non-blocking manner
+                readable = {
+                    masters[0]: sys.stdout.buffer,
+                    masters[1]: sys.stderr.buffer,
+                }
+                while readable:
+                    if stop.isSet():
+                        logger.info("Received stop event")
+                        break
+                    for fd in select(readable, [], [], 0)[0]:
+                        try:
+                            data = os.read(fd, 1024)
+                        except OSError as e:
+                            if (e.errno != errno.EIO): # EIO is an I/O Error
+                                raise
+                            del readable[fd]
+                        else:
+                            # If an empty bytes object is returned it means the EOF has been reached which indicates the process has terminated.
+                            if not data:
+                                del readable[fd]
+                            else:
+                                # Write the data to the corresponding sys stream so that it looks transparent to the user
+                                readable[fd].write(data)
+                                readable[fd].flush()
+
+                if stop.isSet() and proc.poll() == None:
+                    logger.debug("Sending SIGTERM to subprocess")
+                    os.killpg(os.getpgid(proc.pid), SIGTERM)
+                    sleep(0.5)
+                    if proc.poll() == None:
+                        logger.warning("Process is still alive after sending SIGTERM. Sending SIGKILL")
+                        os.killpg(os.getpgid(proc.pid, SIGKILL))
+
+            # If we've reached here, the child process has terminated
+            for fd in masters: os.close(fd)
+            ret_code = proc.returncode
+            logger.info("Process finished with return code %d" % (ret_code))
+
+        except Exception as err:
+            ret_code = -2
+            logger.error("A %s exception was raised while trying to execute command '%s'" % (type(err).__name__, cmd))
+            logger.error("Exception: %s" % (err))
+
+        logger.debug("%s thread finished" % thread_name)
+        return ret_code
+
+    def play(self, audio_file):
+        if not self._play_thread or self._play_thread.is_alive() == False:
+            cmd = self._play_cmd_pfx + audio_file
+            self._play_thread = Thread(target=self._cmd_runner, args=(cmd, self._stop, ), name="Player", daemon=True)
+            self._play_thread.start()
+        else:
+            self._logger.warning("Attempted to call play() while play is in progress")
+
+    def record(self, audio_file):
+        if not self._rcrd_thread or self._rcrd_thread.is_alive() == False:
+            cmd = self._rcrd_cmd_pfx + audio_file
+            self._rcrd_thread = Thread(target=self._cmd_runner, args=(cmd, self._stop, ), name="Recorder", daemon=True)
+            self._rcrd_thread.start()
+        else:
+            self._logger.warning("Attempted to call record() while recording is in progress")
+
+    def stop(self):
+        self._logger.info("Stopping play and record threads")
+        self._stop.set()
+        self._play_thread.join(timeout=self._tjoin_timeout)
+        if self._play_thread.is_alive():
+            raise Exception("Play thread didn't terminate after %ds" % self._tjoin_timeout)
+        self._rcrd_thread.join(timeout=self._tjoin_timeout)
+        if self._rcrd_thread.is_alive():
+            raise Exception("Record thread didn't terminate after %ds" % self._tjoin_timeout)
+        self._stop.clear()
--- phony.py
+++ phony.py
@@ -1,3 +1,76 @@
-import os
+#!/usr/bin/env python
+import logging, util, os, re, signal, code
+from argparse import ArgumentParser, RawTextHelpFormatter
+from time import sleep
+from pathlib import Path
+from datetime import datetime
+from phone import Phone
+from gpiozero import Button
 
-os.system('arecord --format S16_LE --duration=5 --rate 48000 -c2 /home/wtk/Desktop/test.m4a')
+this_script_path     = Path(__file__).resolve()
+this_script_dir      = this_script_path.parent.resolve()
+this_script_filename = this_script_path.name
+output_path_root     = Path(this_script_dir,'out').resolve()
+
+def sigint_handler(signum, frame):
+    logger = logging.getLogger(name=this_script_filename)
+    logger.critical("Received SIGINT. Exiting")
+    exit()
+signal.signal(signal.SIGINT, sigint_handler)
+
+def parse_args():
+    desc_str = ("Implements a telephone message recorder\n\n")
+    parser = ArgumentParser(formatter_class=RawTextHelpFormatter, description=desc_str)
+    parser.add_argument("-o", "--output", type=str, default=output_path_root, help="The directory to output recorded audio files and the log (default=%(default)s)")
+    parser.add_argument("--debug", action="store_true", help="If specified, log messages at DEBUG level and higher will be printed to the console")
+    return parser.parse_args()
+
+# Look through output_path_root to determine the number the last recording started with and generate a new filename
+def get_new_filename(logger, out_dir):
+    logger.debug("Determining next recording filename")
+    try:
+        outdir_contents = os.listdir(path=out_dir)
+    except Exception as err:
+        logger.error("A %s exception was raised while trying to list the contents of directory %s" % (type(err).__name__, outdir_contents))
+        raise
+
+    # Iterate through the list of outdir_contents to find the highest numbered directory
+    last_dir_num = 0
+    for obj in outdir_contents:
+        matched = re.match(pattern="^([0-9]+)_", string=obj)
+
+        # matched[1] corresponds to the first capture group
+        if matched is not None and matched[1] is not None and int(matched[1]) > last_dir_num:
+            last_dir_num = int(matched[1])
+
+    new_file_name = "%d_Recording_%s.wav" % (last_dir_num + 1, datetime.now().strftime("%d%b%y-%H%M%S"))
+    logger.debug("Last recording number was %d. Using new filename %s" % (last_dir_num, new_file_name))
+    return str(Path(out_dir, new_file_name).resolve())
+
+def handset_up(button, phone_manager, logger, out_dir):
+    new_file_name = get_new_filename(logger=logger, out_dir=out_dir)
+    phone_manager.play(audio_file="/home/wtk/Desktop/test.wav")
+    phone_manager.record(audio_file=new_file_name)
+
+def handset_down(button, phone_manager):
+    phone_manager.stop()
+
+if (__name__ == "__main__"):
+    args = parse_args()
+    util.init_logging(console_level=(logging.DEBUG if args.debug else logging.INFO))
+    logger = logging.getLogger(name=this_script_filename)
+
+    start_time = datetime.now()
+    logger.info("Program started at %s" % (start_time.now().strftime("%H:%M:%S on %d %B %Y")))
+
+    phone_manager = Phone()
+    handset_button = Button(pin=14, pull_up=True, bounce_time=0.5)
+    handset_button.when_activated = lambda: handset_up(button=handset_button, phone_manager=phone_manager, logger=logger, out_dir=args.output)
+    handset_button.when_deactivated = lambda: handset_down(button=handset_button, phone_manager=phone_manager)
+
+    # Main thread loop. Nothing to do except wait for a the OS to eventually kill us
+    while True:
+        sleep(10)
+
+    if args.debug:
+        code.interact(banner="\n", local=locals()) # Enter interactive Python interpreter
